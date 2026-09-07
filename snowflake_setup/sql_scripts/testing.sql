22ace5fc47df6d1f16f123363adfaead7d00c6f1
Miranda Lyng <lyngmiranda@gmail.com>
2026-09-07 12:08:59 +0200

    Testing the databases , schemas e.g. in Snowflake

--- snowflake_setup/sql_scripts/testing.sql
+++ snowflake_setup/sql_scripts/testing.sql
@@ -1,4 +1,10 @@
 
 SHOW DATABASES; 
 
-USE SNOWFLAKE_LEARNING_DB; 
+USE snowflake_sample_data; 
+
+SHOW SCHEMAS 
+
+SHOW TABLES IN SCHEMA TPCH_SF1; 
+
+SELECT * FROM snowflake_sample_data.tpch_sf1.customer; 
